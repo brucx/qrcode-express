44fd82c6bad3788174ae97c6ca8edb923f0b2072
Bruce Xiong <xiongty@outlook.com>
2019-12-12 20:59:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# QRCODE-EXPRESS
+
+二维码生成服务
+
+```
+curl --request GET \
+  --url http://localhost:3000/qrcode?content=http://www.woodylibrary.com
+```
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import * as express from 'express';
+import * as QRCode from 'qrcode';
+import Pino = require('express-pino-logger');
+
+const pino = Pino();
+
+/**
+ * WEB 服务
+ */
+const app = express();
+
+app.use(pino);
+
+app.post('/', async (req, resp) => {
+  const { body } = req;
+  pino.logger.info({ body });
+  resp.send('OK');
+});
+
+/**
+ * 生成二维码
+ */
+app.get('/qrcode', async (req, resp) => {
+  const {content} = req.query;
+  pino.logger.info({ content });
+  try {
+    resp.writeHead(200, { 'Content-Type': 'image/png' });
+    QRCode.toFileStream(resp, content);
+  } catch (error) {
+    pino.logger.error(error);
+    resp.send({ error });
+  }
+});
+
+const server = app.listen(3000, '0.0.0.0', () => {
+  pino.logger.info('无头浏览器海报渲染服务已启动, 地址是：http://0.0.0.0:3000');
+});
